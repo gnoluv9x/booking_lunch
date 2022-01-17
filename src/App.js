1f0b4b66d5f8aa7f1611eee8ffc6267533ece3eb
vulong <vulongpt@gmail.com>
2022-01-17 16:59:30 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Col, Row } from "antd";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <Row>
+                <Col span={12}>1</Col>
+                <Col span={12}>2</Col>
+            </Row>
+        </div>
+    );
+}
+
+export default App;
