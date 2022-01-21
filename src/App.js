19c0f990eb458f4ed34e7f0f8ee43490d0023da7
manhkien198 <manhkien1998@gmail.com>
2022-01-21 17:03:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Col, Row } from "antd";
-import "./App.scss";
+import Login from "./feature/validate/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./feature/validate/ForgotPassword";
+import RePassword from "./feature/validate/RePassword";
 
 function App() {
-    return (
-        <div className="App">
-            <Row>
-                <Col span={12}>1</Col>
-                <Col span={12}>2</Col>
-            </Row>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
+        <Route path="/repassword" element={<RePassword />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
